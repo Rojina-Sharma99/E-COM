f451fbfa596de39511eddd2c4250f8af18bf0da0
Rojina-Sharma99 <sharmarojina99@gmail.com>
2025-04-03 20:25:38 +1030

    Added home page extra features
    added items image scrollable feature with selecting, color, sizes and quantity.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Layout/Hero";
 import GenderCollectionSection from "../components/Products/GenderCollection";
 import NewArrivals from "../components/Products/NewArrivals";
+import ProductDetails from "../components/Products/ProductDetails";
 
 const Home = () => {
   return (
@@ -8,6 +9,10 @@ const Home = () => {
       <Hero />
       <GenderCollectionSection />
       <NewArrivals />
+
+      {/**Best seller section */}
+      <h2 className="mb-4 text-3xl font-bold text-center">BestSeller</h2>
+      <ProductDetails/>
     </div>
   );
 };
