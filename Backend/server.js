8251bc12af01fa38a8fa94ed7940a3058b93f447
Rojina-Sharma99 <sharmarojina99@gmail.com>
2025-04-25 11:13:36 +0930

    created db.js, user.js and connected mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
 const app = express();
+app.use(express.json());
 
 //to allow cors origin , allowing difference instance fromdifffefent resources
 app.use(cors());
+dotenv.config();
+console.log(process.env.PORT, "PORT");
 
-const PORT = 9000;
+//define in env file port if not then use 3000 port
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+//sending response to get response in index
+app.get("/", (req, res) => {
+  res.send("Welcome to Ecom website API");
+});
 
 //checking to see if port is running
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
